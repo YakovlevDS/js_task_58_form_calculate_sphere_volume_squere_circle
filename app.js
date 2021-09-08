240cdf342944898768f9427121cf05a1b1b04118
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-09-08 20:54:47 +0200

    added refactoring

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
-// ? Task: Write a JavaScript program to calculate the volume of a sphere.
+// ? Task: Write a JavaScript program to calculate the volume of a sphere and
 
 
 
 // Solution 1
-const buttonVolume = document.querySelector("#calcVolume");
-const buttonSquere = document.querySelector("#calcSquere");
+const btnVolume = document.querySelector("#calcVolume");
+const btnSquere = document.querySelector("#calcSquere");
 const volume = document.querySelector("#volume");
 const squere = document.querySelector("#squere");
-let r;
 
-const volumeSphere = () => {
-    r = Math.abs(document.querySelector("#radius").value);
-        return (volume.value = ((4 / 3) * Math.PI * Math.pow(r, 3))
-            .toFixed(2));
-}
+const r = () => Math.abs(document.querySelector("#radius").value);
 
-const squereCircle = () => {
-    r = Math.abs(document.querySelector("#radius").value);
-    return squere.value = (2 * Math.PI * Math.pow(r, 2))
+const volumeSphere = () => volume.value = ((4 / 3) * Math.PI * Math.pow(r(), 3))
+            .toFixed(2);
+
+const squereCircle = () => squere.value = (2 * Math.PI * Math.pow(r(), 2))
         .toFixed(2);
-}
 
-
-buttonVolume.addEventListener("click", volumeSphere);
-buttonSquere.addEventListener("click", squereCircle);
+btnVolume.addEventListener("click", volumeSphere);
+btnSquere.addEventListener("click", squereCircle);
 
 
 // ! Explanation: 
