c2e4facb11fc76f54705f256614f535e5232da1b
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-09-08 17:19:44 +0200

    added solution

---
+++ app.js
@@ -0,0 +1,21 @@
+// ? Task:
+Write a JavaScript program to calculate the volume of a sphere.
+
+
+
+// Solution 1
+
+function volume_sphere()
+ {
+  var volume;
+  var radius = document.getElementById('radius').value;
+  radius = Math.abs(radius);
+  volume = (4/3) * Math.PI * Math.pow(radius, 3);
+  volume = volume.toFixed(4);
+  document.getElementById('volume').value = volume;
+  return false;
+ } 
+window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+
+
+// ! Explanation: 
